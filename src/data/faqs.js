b8691ff1fcf0afc035c6c718f7a8a64ff12eab28
Gagas Nandaka <nandakagagas@gmail.com>
2023-08-23 15:01:23 +0700

    fnal fix

--- src/data/faqs.js
+++ src/data/faqs.js
@@ -6,22 +6,22 @@ const faqs = [
     },
     {
         id: 2,
-        title: "Dimana tempat untuk mengunduh aplikasi RakitPC?",
+        title: "Dimana tempat untuk mengunduh RakitPC?",
         description: "Dapat diunduh di Google Play Store search keyword 'rakit pc' atau dengan menggunakan link intip.in/rakitpc",
     },
     {
         id: 3,
-        title: "Bagaimana untuk melakukan konseling dengan RakitPC?",
+        title: "Bagaimana untuk melakukan konseling?",
         description: "Dengan klik button floating di pojok kanan bawah atau klik menu di navbar atas untuk melakukan konsultasi gratis.",
     },
     {
         id: 4,
-        title: "Darimana RakitPC mendapatkan rekomendasi rakitan komputer?",
+        title: "Darimana RakitPC mendapatkan rakitan?",
         description: "Kami berkerja sama dengan toko komputer partner kami dan dari rekomendasi spesifikasi tersbut kami rangking mana yang men-deliver value terbaik untuk customer.",
     },
     {
         id: 5,
-        title: "Apakah RakitPC dan layanan Konsultasi berbayar?",
+        title: "Apakah RakitPC & layanan Konsultasi berbayar?",
         description: "Tidak, Aplikasi RakitPC Gratis digunakan beserta juga layanan konsultasi, Kecuali jika ingin melakukan pembelian komputer.",
     },
     {
--- src/layouts/Header.vue
+++ src/layouts/Header.vue
@@ -12,7 +12,7 @@
     <!-- Navbar Start -->
     <div class="container mx-auto px-5">
         <header class="flex justify-between py-5 items-center">
-            <div class="h-6 w-40">
+            <div class="h-10 w-40">
                 <a href="/"><img src="/images/logo-final.png" alt="Site Logo"></a>
             </div>
             
